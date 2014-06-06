68017fdecb310406ae989ea5a3ac7ae234d80c53
mauwot <meine.mail@mail.de>
2014-06-07 00:22:46 +0200

    legend text was out of bound in Rstudio after exporting PNG manually skript did work

--- plot3.R
+++ plot3.R
@@ -9,7 +9,7 @@ subdat <- subset(data, pt>=strptime(20070201,format="%Y%m%d")& pt<=strptime(2007
 pt_sub <- strptime(paste(subdat$Date,subdat$Time),"%d/%m/%Y %H:%M:%S")
 
 #transperent background
-par(bg=NA, mar=c(4,4,4,4), mai = c(1,1,1,0.1))
+par(bg=NA, cex=1)
 #set to us convention
 Sys.setlocale("LC_TIME","us")
 plot(pt_sub,as.numeric(subdat$Sub_metering_1), ylab="Energy sub metering", xlab="", type="n")
@@ -18,7 +18,7 @@ lines(pt_sub,as.numeric(subdat$Sub_metering_2), col="red")
 lines(pt_sub,as.numeric(subdat$Sub_metering_3), col = "blue")
 
 #legend
-legend(pt_sub[1700],40,c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1), col = c("black","red","blue")) 
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1), col = c("black","red","blue"),cex=0.7) 
 
 #copy to png
 dev.copy(png, "./figure/plot3.png", width=480, height=480)
